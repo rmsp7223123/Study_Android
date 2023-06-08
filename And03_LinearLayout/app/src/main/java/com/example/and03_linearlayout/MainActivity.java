887ed0e57cd35a4b4ea6dc8e679dfcac5c0dc68c
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-08 14:07:55 +0900

    ++

---
+++ And03_LinearLayout/app/src/main/java/com/example/and03_linearlayout/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.and03_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lineartest);
+//        LinearLayout line1 = findViewById(R.id.line1);
+//        Log.i("문병준", "로그 Info ");
+//        Log.w("문병준", "경고 ", null);
+//        if(line1 != null) {
+////            System.out.println(); Android에서는 syso를 사용하지 않음 log 사용
+//            Log.d("문병준", "onCreate:Linear가 null이 아님 ");
+//
+//        } else {
+////            Log.d("문병준", "onCreate:Linear가 null임 ");
+//            Log.e("문병준", "onCreate:123123123 ", null );
+//        }
+
+    }
+}
