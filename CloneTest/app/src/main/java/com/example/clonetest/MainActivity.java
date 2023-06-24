d22626923775aa41daeea5a028603bc5fa8e24cc
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-24 11:54:50 +0900

    ++

--- CloneTest/app/src/main/java/com/example/clonetest/MainActivity.java
+++ CloneTest/app/src/main/java/com/example/clonetest/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.clonetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.clonetest.databinding.ActivityMainBinding;
+import com.example.clonetest.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnLogin.setOnClickListener(view -> {
+            intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
