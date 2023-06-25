e9469e2ea4cb0fd383cc525401097b10c4dac461
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-25 19:00:00 +0900

    ++

--- CloneTest/app/src/main/java/com/example/clonetest/MainActivity.java
+++ CloneTest/app/src/main/java/com/example/clonetest/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.clonetest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.clonetest.databinding.ActivityMainBinding;
 import com.example.clonetest.ui.login.LoginActivity;
+import com.example.clonetest.ui.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
     Intent intent;
+
+    FragmentManager manager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,6 @@ public class MainActivity extends AppCompatActivity {
             intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
         });
+        manager.beginTransaction().replace(R.id.container_linear, new LoginFragment()).commit();
     }
 }
