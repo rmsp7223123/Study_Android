ae4d46d620f33ea7d5a48ff14d20aa0ef65419a2
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-17 21:39:26 +0900

    ++

--- Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
+++ Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.example.project02_lastproject.member;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.project02_lastproject.MainActivity;
@@ -12,6 +18,21 @@ import com.example.project02_lastproject.common.CommonConn;
 import com.example.project02_lastproject.common.CommonVar;
 import com.example.project02_lastproject.databinding.ActivityLoginBinding;
 import com.google.gson.Gson;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+import com.navercorp.nid.NaverIdLoginSDK;
+import com.navercorp.nid.oauth.NidOAuthLogin;
+import com.navercorp.nid.oauth.OAuthLoginCallback;
+import com.navercorp.nid.profile.NidProfileCallback;
+import com.navercorp.nid.profile.data.NidProfileResponse;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +48,23 @@ public class LoginActivity extends AppCompatActivity {
         binding.btnLogin.setOnClickListener(v -> {
             login();
         });
+
+        NaverIdLoginSDK.INSTANCE.initialize(this, "", "", getString(R.string.app_name));
+//        naverLogin();
+
+        KakaoSdk.init(this, "faf601ce16c809ff700bf54d00c05061");
+        UserApiClient.getInstance().unlink(new Function1<Throwable, Unit>() {
+            @Override
+            public Unit invoke(Throwable throwable) {
+                return null;
+            }
+        });
+
+        binding.btnLoginKakao.setOnClickListener(v -> {
+            kakaoLogin(this);
+        });
+
+
     }
 
 
@@ -50,5 +88,102 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void kakaoLogin(Context context){
+        KakaoSdk.init(this, "");
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인(웹뷰)
+        // Kotlin은 경우에 따라서 생성자 대신에 메소드로 객체를 return받음. UserApiClient name = new ... x -> UserApiClient.{static UserApiClient.method}
+
+        Function2<OAuthToken, Throwable, Unit> callback = (token, error) -> {
+            if(error != null){
+                Log.e("카카오 로그인 에러", "invoke: "+error.getMessage());
+            }else {
+                Log.d("카카오 로그인 성공", "invoke: "+token.toString());
+                getKakaoProfile();
+            }
+            return null;
+        };
+
+        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(context)){
+            Log.d("카카오", "isKakaoTalkLoginAvailable: true");
+            UserApiClient.getInstance().loginWithKakaoTalk(context, callback);
+        }else{
+            Log.d("카카오", "isKakaoTalkLoginAvailable: false");
+            UserApiClient.getInstance().loginWithKakaoAccount(context, callback);
+        }
+    }
+
+    public void getKakaoProfile(){
+        UserApiClient.getInstance().me((user, error) -> {
+            if(error == null){
+                Log.d("카카오", "getKakaoProfile: "+user.toString());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getEmail());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getNickname());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getThumbnailImageUrl());
+            }
+            return null;
+        });
+    }
+
+//    public void naverLogin(){
+//        binding.btnLoginNaver.setOAuthLogin(new OAuthLoginCallback() {
+//            @Override
+//            public void onSuccess() {
+//                Log.d("네이버", "onSuccess: "+NaverIdLoginSDK.INSTANCE.getAccessToken());
+//                new NidOAuthLogin().callProfileApi(new NidProfileCallback<NidProfileResponse>() {
+//                    @Override
+//                    public void onSuccess(NidProfileResponse res) {
+//                        Log.d("네이버", "onSuccess: "+res.getProfile().getEmail());
+//                        Log.d("네이버", "onSuccess: "+res.getProfile().getNickname());
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int i, @NonNull String s) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(int i, @NonNull String s) {
+//
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onFailure(int i, @NonNull String s) {
+//                Log.d("네이버", "onFailure: ");
+//            }
+//
+//            @Override
+//            public void onError(int i, @NonNull String s) {
+//                Log.d("네이버", "onError: ");
+//            }
+//        });
+//    }
+
+
+
+
+    public static String getKeyHash(final Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            if (packageInfo == null)
+                return null;
+
+            for (Signature signature : packageInfo.signatures) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
