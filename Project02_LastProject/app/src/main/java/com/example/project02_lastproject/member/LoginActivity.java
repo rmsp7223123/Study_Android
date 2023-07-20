b66dd4d3a610b19286c72ac1ffc42323f97aeb86
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-20 20:10:45 +0900

    ++

--- Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
+++ Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
@@ -1,16 +1,20 @@
 package com.example.project02_lastproject.member;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -42,6 +46,9 @@ public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
 
+    SharedPreferences pref; //자바 변수가 static 등의 멤버로 데이터를 저장하면 앱을 다시 실행시 처음부터 불러옴. (DB)
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,7 +73,20 @@ public class LoginActivity extends AppCompatActivity {
             kakaoLogin(this);
         });
 
-        checkPermission();
+        pref = getPreferences(MODE_PRIVATE);
+        editor = pref.edit(); //데이터를 넣거나 수정하기위한 객체
+
+        pref.getInt("permission", -1);
+
+        //최초 실행 시 숫자, -1
+        //한번 실행 후 -> ?
+        //두번 실행 후 -> ?
+        //거절 ->
+        //승인 ->
+
+        if (pref.getInt("permission", -1) == -1) {
+            checkPermission();
+        }
 
 
     }
@@ -94,52 +114,52 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void kakaoLogin(Context context){
+    public void kakaoLogin(Context context) {
         KakaoSdk.init(this, "faf601ce16c809ff700bf54d00c05061");
         // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인(웹뷰)
         // Kotlin은 경우에 따라서 생성자 대신에 메소드로 객체를 return받음. UserApiClient name = new ... x -> UserApiClient.{static UserApiClient.method}
 
         Function2<OAuthToken, Throwable, Unit> callback = (token, error) -> {
-            if(error != null){
-                Log.e("카카오 로그인 에러", "invoke: "+error.getMessage());
-            }else {
-                Log.d("카카오 로그인 성공", "invoke: "+token.toString());
+            if (error != null) {
+                Log.e("카카오 로그인 에러", "invoke: " + error.getMessage());
+            } else {
+                Log.d("카카오 로그인 성공", "invoke: " + token.toString());
                 getKakaoProfile();
             }
             return null;
         };
 
-        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(context)){
+        if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(context)) {
             Log.d("카카오", "isKakaoTalkLoginAvailable: true");
             UserApiClient.getInstance().loginWithKakaoTalk(context, callback);
-        }else{
+        } else {
             Log.d("카카오", "isKakaoTalkLoginAvailable: false");
             UserApiClient.getInstance().loginWithKakaoAccount(context, callback);
         }
     }
 
-    public void getKakaoProfile(){
+    public void getKakaoProfile() {
         UserApiClient.getInstance().me((user, error) -> {
-            if(error == null){
-                Log.d("카카오", "getKakaoProfile: "+user.toString());
-                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getEmail());
-                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getNickname());
-                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getThumbnailImageUrl());
+            if (error == null) {
+                Log.d("카카오", "getKakaoProfile: " + user.toString());
+                Log.d("카카오", "getKakaoProfile: " + user.getKakaoAccount().getEmail());
+                Log.d("카카오", "getKakaoProfile: " + user.getKakaoAccount().getProfile().getNickname());
+                Log.d("카카오", "getKakaoProfile: " + user.getKakaoAccount().getProfile().getThumbnailImageUrl());
             }
             return null;
         });
     }
 
-    public void naverLogin(){
+    public void naverLogin() {
         binding.btnLoginNaver.setOAuthLogin(new OAuthLoginCallback() {
             @Override
             public void onSuccess() {
-                Log.d("네이버", "onSuccess: "+NaverIdLoginSDK.INSTANCE.getAccessToken());
+                Log.d("네이버", "onSuccess: " + NaverIdLoginSDK.INSTANCE.getAccessToken());
                 new NidOAuthLogin().callProfileApi(new NidProfileCallback<NidProfileResponse>() {
                     @Override
                     public void onSuccess(NidProfileResponse res) {
-                        Log.d("네이버", "onSuccess: "+res.getProfile().getEmail());
-                        Log.d("네이버", "onSuccess: "+res.getProfile().getNickname());
+                        Log.d("네이버", "onSuccess: " + res.getProfile().getEmail());
+                        Log.d("네이버", "onSuccess: " + res.getProfile().getNickname());
                     }
 
                     @Override
@@ -167,8 +187,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
-
     public static String getKeyHash(final Context context) {
         PackageManager pm = context.getPackageManager();
         try {
@@ -192,27 +210,73 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private final int REQ_PERMISSION = 1000;
-    private void checkPermission(){
+    private final int REQ_PERMISSION_DENY = 1001;
+
+    private void checkPermission() {
+        int permission = pref.getInt("permission", -1);
+        permission++;
+        editor.putInt("permission", permission);
+        editor.apply();
         String[] permissions = {Manifest.permission.CAMERA,
-                Manifest.permission.ACCESS_MEDIA_LOCATION
+                Manifest.permission.ACCESS_MEDIA_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
         };//카메라 권한을 스트링으로 가져옴.
         // ContextCompat(액티비티가 아닌 곳), ActivityCompat(액티비티)
-        for(int i=0; i<permissions.length; i++){
+        for (int i = 0; i < permissions.length; i++) {
             //내가 모든 권한이 필요하다면 전체 권한을 하나씩 체크해서 허용 안됨이 있는 경우 다시 요청을 하게 만든다.
-            if(ActivityCompat.checkSelfPermission(this, permissions[0])== PackageManager.PERMISSION_DENIED){
-                ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION);
+            if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_DENIED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {
+                    //최초 앱이 설치되고 실행 시 false가 나옴. => 사용자가 거부 후 true 제거부 => false
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("권한 요청").setMessage("권한이 반드시 필요합니다. \n미허용 시 앱 사용 불가");
+                    builder.setPositiveButton("확인(권한 허용)", (dialog, which) -> {
+                        //2번 권한 설명 후 다시보여줌.
+                        ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION_DENY);
+                    });
+                    builder.setNegativeButton("종료(권한 허용 불가)", (dialog, which) -> {
+                        finish();
+                    });
+
+                    builder.create().show(); //<==넣어줘야함
+                } else {
+                    //1번
+                    ActivityCompat.requestPermissions(this, permissions, REQ_PERMISSION);
+                }
             }
             break;
         }
     }
 
 
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(REQ_PERMISSION==requestCode){
+        if (REQ_PERMISSION == requestCode) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    //거절된 권한이 있음.
+                    checkPermission();
+                    break;
+                }
+            }
             Log.d("권한", "onRequestPermissionsResult: 권한 요청 완료");
+        } else if (REQ_PERMISSION_DENY == requestCode) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    Log.d("권한", "onRequestPermissionsResult: 다시 권한요청 화면을 띄울 수가 없음. 2회 거절당함");
+                    editor.putInt("permission", -2);
+                    //3번
+                    viewSetting();
+//                    checkPermission();
+                }
+            }
         }
+    }
+
+    public void viewSetting() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+        startActivity(intent);
     }
 }
